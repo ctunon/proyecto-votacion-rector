a972431fd271c45594344f2ea5b24a28aeb24b94
Carlos Tuñón <ctunon@ayudinga.org>
2022-09-21 01:48:38 -0500

    enlace de elementos de ventana - main activity

--- app/src/main/java/com/example/proyecto_votacion_rector/MainActivity.java
+++ app/src/main/java/com/example/proyecto_votacion_rector/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.proyecto_votacion_rector;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import java.util.ArrayList;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnIngresar;
+    EditText etxCedula;
+
+    ArrayList<Datos> votantes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIngresar = findViewById(R.id.btnIngresar);
+        etxCedula = findViewById(R.id.etxCedula);
+
+
+
     }
+
+
+
+
 }
