f0b2c57eefbdb5f784c14dac5a02e593fe91dfe5
Carlos Tuñón <ctunon@ayudinga.org>
2022-09-21 01:49:14 -0500

    creacion de actividades - resultados/votacion

---
+++ app/src/main/java/com/example/proyecto_votacion_rector/Resultados.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_votacion_rector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Resultados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultados);
+    }
+}
